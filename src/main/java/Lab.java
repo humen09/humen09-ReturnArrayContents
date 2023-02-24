10da575c782eb1c643d03d2f0723072bb57539dd
humen09 <humen08@gmail.com>
2023-02-24 23:09:06 +0000

    completed arrayToString Method

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,9 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String j = "";
+        for(int i:nums)
+j += i +" ";
+        return j;
     }
 }
